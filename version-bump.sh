b41761b9faa7bcd0903aedacc7f42191f06a4e36
WebDevGeek <pawel.marszalec92@gmail.com>
2020-07-16 15:23:55 +0200

    packages

--- version-bump.sh
+++ version-bump.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
-echo "release version bump"
+# echo "release version bump"
 # git config --global user.email "travis@travis-ci.org"
 # git config --global user.name "Travis CI"
-git config --global push.default current
-git checkout ${TRAVIS_BRANCH}
-if [[ $TRAVIS_BRANCH == "staging" ]]
-then
-    yarn version --patch --message "New patch version relesed [skip ci]"
-elif [[ $TRAVIS_BRANCH == "master" ]]
-then
-    yarn version --minor --message "New minor version relesed [skip ci]"
-else
-    echo "version update not needed on this branch"
-fi
+# git config --global push.default current
+# git checkout ${TRAVIS_BRANCH}
+# if [[ $TRAVIS_BRANCH == "staging" ]]
+# then
+#     yarn version --patch --message "New patch version relesed [skip ci]"
+# elif [[ $TRAVIS_BRANCH == "master" ]]
+# then
+#     yarn version --minor --message "New minor version relesed [skip ci]"
+# else
+#     echo "version update not needed on this branch"
+# fi
 # git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags
-release_params = '{
-  "tag_name": "v6.0.0",
+
+PARAMS='{
+  "tag_name": "v0.5.1",
   "target_commitish": "master",
-  "name": "v6.0.0",
+  "name": "v5.0.0",
   "body": "Description of the release",
   "draft": false,
   "prerelease": false
 }'
 
-curl -s -X POST -d $release_params https://api.github.com/repos/WebDevGeek9922/travis-test/releases
+curl -s -X POST -d $PARAMS https://api.github.com/repos/WebDevGeek9922/travis-test/releases

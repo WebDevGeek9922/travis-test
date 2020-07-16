3051bd6df9e54f6470759802e6333d2747b79ed1
WebDevGeek <pawel.marszalec92@gmail.com>
2020-07-16 17:27:46 +0200

    fix

--- version-bump.sh
+++ version-bump.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-echo "release version bump"
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git config --global push.default current
-git checkout ${TRAVIS_BRANCH}
-if [[ $TRAVIS_BRANCH == "staging" ]]
-then
-    yarn version --patch --message "New patch version relesed [skip ci]"
-elif [[ $TRAVIS_BRANCH == "master" ]]
-then
-    yarn version --minor --message "New minor version relesed [skip ci]"
-else
-    echo "version update not needed on this branch"
-fi
-git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags
+# echo "release version bump"
+# git config --global user.email "travis@travis-ci.org"
+# git config --global user.name "Travis CI"
+# git config --global push.default current
+# git checkout ${TRAVIS_BRANCH}
+# if [[ $TRAVIS_BRANCH == "staging" ]]
+# then
+#     yarn version --patch --message "New patch version relesed [skip ci]"
+# elif [[ $TRAVIS_BRANCH == "master" ]]
+# then
+#     yarn version --minor --message "New minor version relesed [skip ci]"
+# else
+#     echo "version update not needed on this branch"
+# fi
+# git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags
 
 # git_tag=`git describe --tags`
 

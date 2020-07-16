6e1d1a637f751e6009fff8438454da63038773bf
WebDevGeek <pawel.marszalec92@gmail.com>
2020-07-16 14:09:03 +0200

    fix

--- version-bump.sh
+++ version-bump.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "release version bump"
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+# git config --global user.email "travis@travis-ci.org"
+# git config --global user.name "Travis CI"
 git config --global push.default current
 git checkout ${TRAVIS_BRANCH}
 if [[ $TRAVIS_BRANCH == "staging" ]]
@@ -14,4 +14,14 @@ then
 else
     echo "version update not needed on this branch"
 fi
-git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags
+# git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags
+release_params = '{
+  "tag_name": "v6.0.0",
+  "target_commitish": "master",
+  "name": "v6.0.0",
+  "body": "Description of the release",
+  "draft": false,
+  "prerelease": false
+}'
+
+curl -s -X POST -d $release_params https://api.github.com/repos/WebDevGeek9922/travis-test/releases

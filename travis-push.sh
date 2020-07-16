6b68d47eaa4afe15c7e4c9fddd9e5214ae7b0574
WebDevGeek <pawel.marszalec92@gmail.com>
2020-07-16 11:48:54 +0200

    fix

--- travis-push.sh
+++ travis-push.sh
@@ -7,8 +7,5 @@ git config --global push.default current
 git stash
 git checkout ${TRAVIS_BRANCH}
 git stash pop
-yarn version --patch
-git add package.json yarn.lock
-git add yarn.lock
-git commit -m "[skip ci] New version released"
+yarn version --patch --message "New version relesed [skip ci]"
 git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git --follow-tags

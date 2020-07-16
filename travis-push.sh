cf527c14f4524c1e6fb7aea85d0868f3a8422d76
WebDevGeek <pawel.marszalec92@gmail.com>
2020-07-16 10:59:04 +0200

    fix

---
+++ travis-push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "git push"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git stash
+git checkout ${TRAVIS_BRANCH}
+git stash pop
+yarn version --patch
+git push https://${GITHUB_TOKEN}@github.com/WebDevGeek9922/travis-test.git
